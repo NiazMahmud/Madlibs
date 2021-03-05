487c73c8d4a4d6e39d363374622f8365e20826ba
NiazMahmud <s.m.mahmud@northsouth.edu>
2021-03-05 16:06:41 +0600

    2nd update

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+# STRING CONCATENATION (aka how to put strings together)
+# suppose we eant to create a string that says " weclome to python world _____"
+
+name="ronok"
+
+# a few to do this
+print("weclome to python world " + name )
+print("weclome to python world {}" .format(name))
+print(f"weclome to python world {name}" )
